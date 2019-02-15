8a389ed3a682479984f53abf7e708f6c42511695
darDomanski <dardomanski@gmail.com>
2019-02-15 11:58:43 +0100

    Implement Node for Queue

--- src/main/java/com/codecool/Queue/Node.java
+++ src/main/java/com/codecool/Queue/Node.java
@@ -1,4 +1,28 @@
 package com.codecool.Queue;
 
 public class Node {
+    private String value;
+    private Node nextNode;
+    private int priority;
+
+    public Node(String value, int priority) {
+        this.value = value;
+        this.priority = priority;
+    }
+
+    public String getValue() {
+        return this.value;
+    }
+
+    public Node getNextNode() {
+        return this.nextNode;
+    }
+
+    public void setNextNode(Node node) {
+        this.nextNode = node;
+    }
+
+    public int getPriority() {
+        return priority;
+    }
 }
